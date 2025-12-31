7808d02b1a9576bc1a1873fb29c3b5ec992f6b82
breadgrocery <breadgrocery@gmail.com>
2025-12-31 11:08:01 +0800

    feat(create-template): support code splitting for ESM import
    https://github.com/babalae/better-genshin-impact/pull/2494

--- packages/create-script/template/bettergi.config.ts
+++ packages/create-script/template/bettergi.config.ts
@@ -7,19 +7,20 @@ export default defineConfig({
   assetsDir: "assets",
   outDir: "dist",
   additionalFiles: ["README.md", "LICENSE"], // 需要额外打包的文件
+  codeSplitting: true, // 启用代码分割（要求 `bgi_version` >= `0.54.0`）
   minify: false,
   banner: true,
+  // 清单信息
+  manifest: {
+    name: "{{description}}",
+    bgi_version: "0.54.0", // 该脚本适用的 BetterGI 最低版本
+    saved_files: ["store/*.json"] // 脚本升级时需保留还原的文件/文件夹
+  },
+  // UI配置
+  settings,
   // 调试配置
   bettergi: {
     enable: true,
     outDir: "{{outDir}}"
-  },
-  // 清单信息
-  manifest: {
-    name: "{{description}}",
-    bgi_version: "0.53.0", // 该脚本适用的 BetterGI 最低版本
-    saved_files: ["store/*.json"] // 脚本升级时需保留还原的文件/文件夹
-  },
-  // UI配置
-  settings
+  }
 });
