32a6937cbdae187181f714436040dd06541cbd7c
breadgrocery <breadgrocery@gmail.com>
2026-01-16 16:37:01 +0800

    fix(cli): remove version info from chunks when using pnpm

--- packages/cli/src/plugins/post-build-processor.ts
+++ packages/cli/src/plugins/post-build-processor.ts
@@ -5,6 +5,12 @@ import { type ConfigManager } from "../config/index.js";
 const PostBuildProcessor = (manager: ConfigManager): RolldownPlugin => {
   return {
     name: "post-build-processor",
+    renderChunk(code) {
+      return code.replace(/^\/\/#region (.*)$/gm, (full, group) => {
+        const i = group.lastIndexOf("node_modules");
+        return i === -1 ? full : full.replace(group, group.slice(i));
+      });
+    },
     async writeBundle() {
       await postBuild(manager);
     }
